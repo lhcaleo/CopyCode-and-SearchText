52f514aac564fccd74788c59c14bf4a0d42bd2ee
hao <lihaocaleo@gmail.com>
2019-07-04 02:09:41 -0400

    button created, need adjust position

--- src/content.js
+++ src/content.js
@@ -3,8 +3,30 @@
 chrome.runtime.sendMessage({
     todo: "showPageAction"
 });
+alert("Hello from content.js")
+$('span').css('color', "red");
+$('code').css("font-size","15px")
+var btn = document.createElement("button");
+btn.innerHTML="copy";
+btn.setAttribute("class","copyButton");
+$('code').append(btn);
 
-alert("Hello");
+var css = {
+        position: 'relative',
+        top: '0.5rem',
+        right: '0.5rem',
+        //z-index: '10',
+        display: 'block',
+        padding: '0.25rem 0.5rem',
+        color: 'DimGrey',
+        'background-color': 'transparent',
+        border: '0'
+}
+$('.copyButton').css(css);
 
-
-$("p").hide();
+//$('pre.default.prettyprint.prettyprinted').hide();
+//$('pre.default.prettyprint.prettyprinted').css("font-size","5px")
+//$('div.user-info').hide();
+//$('div.user-hover').hide();
+//$('span').appendTo('.user-info user-hover');
+//$('button').hide();
--- src/eventPage.js
+++ src/eventPage.js
@@ -4,19 +4,21 @@
 // @sender -MessageSender
 // @sendResponse -Function to call (at most once) when you have a response. 
 //                The argument should be any JSON-ifiable object.
+
+// To ask chrome to retrieve all tabs so that we can show the icon
+// Get the tab that is active and in current window 
+// Show the page action by pageAction api
+
+
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.todo == "showPageAction") {
-        // To ask chrome to retrieve all tabs so that we can show the icon
-        // Get the tab that is active and in current window 
-        // Show the page action by pageAction api
         chrome.tabs.query({
             active: true,
             currentWindow: true
         }, function (tabs) {
+            alert("Hello from eventPage.js");
             chrome.pageAction.show(tabs[0].id);
         });
     }
-})
-// $(function(){
-//     $("#question-header").text("AAAA");
-// });
+});
