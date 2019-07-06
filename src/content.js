e191dcb2552197de7bd748435cdeb5b083e37121
hao <lihaocaleo@gmail.com>
2019-07-05 22:30:24 -0400

    add tooltip successfully

--- src/content.js
+++ src/content.js
@@ -1,23 +1,33 @@
+ // include bootstrap
+ var script = document.createElement("script");
+ script.src = "bootstrap.bundle.min.js";
+ document.head.appendChild(script);
+ 
 // A javascript runs on the particular url
 // content script send message to show page action
 chrome.runtime.sendMessage({
     todo: "showPageAction"
 });
 
+// Select all the code blocks, and add button for each of them
 var block = document.querySelectorAll("pre > code");
 block.forEach(addButton);
 
 function addButton(block){
     var btn = document.createElement("button");
-    btn.className = "copyButton";
     btn.type = "button"; //The button is a clickable button
+    btn.className = "copyButton";
+    btn.setAttribute("data-toggle","tooltip");
+    btn.setAttribute("data-placement","top")
+    btn.setAttribute("title", "Copy to clipboard");
     btn.innerHTML = "Copy";
     var pre = block.parentNode;
     pre.parentNode.insertBefore(btn, pre);
 }
 
+// CSS styling for the button
 var css = {
-        'font-size': '1.1rem',
+        'font-size': '13px',
         display: 'block',
         'z-index': '5',
         top: '2.7rem',
@@ -34,6 +44,17 @@ var hoverCSS = {
     'background-color':'#007bff',
     cursor: 'pointer'
 }
+var tooltipCSS = {
+    'background-color':'#555',
+    border: '2px'
+}
+
+// Activate tooltip of the button
+$(function() {
+    $('.copyButton').tooltip();
+});
+
+// Define CSS of button by event
 $('.copyButton').css(css);
 $('.copyButton').mouseover(function(){
     $('.copyButton').css(hoverCSS);
@@ -41,3 +62,10 @@ $('.copyButton').mouseover(function(){
 $('.copyButton').mouseout(function(){
     $('.copyButton').css(css);
 });
+
+//TODO:
+/**
+ * 1. Function copy to clipboard
+ * 2. popup a message "copied!" when button is clicked
+ */
+
